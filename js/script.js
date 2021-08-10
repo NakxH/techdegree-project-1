d72c5f7435a26f6d33dddd38b9b3c734689cbc63
Hakan <74737999+NakxH@users.noreply.github.com>
2021-08-11 00:30:13 +0100

    Added auto quote update and random colour

--- js/script.js
+++ js/script.js
@@ -4,13 +4,10 @@ project 1 - A Random Quote Generator
 ******************************************/
 
 // For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+// Check the "Project Resources" section of the project instructions
+// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
-***/
-
+/* This is an array of objects that holds a quote and a source. Optional properties are citation, year and tags.*/
 const quotes = [
   {
     quote: 'The greatest glory in living lies not in never falling, but in rising every time we fall.',
@@ -49,51 +46,75 @@ const quotes = [
 ];
 
 /***
- * `getRandomQuote` function
+ * `getRandomQuote` function.
+ * This function will return a random quote from the quotes array.
 ***/
-
-function getRandomQuote () {
-
-  const randomNumber = Math.floor (Math.random() * quotes.length);
-
+function getRandomQuote() {
+  const randomNumber = Math.floor(Math.random() * quotes.length);
   return quotes[randomNumber];
-
 }
 
 /***
- * `printQuote` function
+ * `printQuote` function.
+ *  This function prints the random quote to the browser, including all of the properties.
 ***/
-
-function printQuote () {
-
+function printQuote() {
   const randomQuote = getRandomQuote();
 
   let html = `
     <p class="quote">${randomQuote.quote}</p>
     <p class="source">${randomQuote.source}
   `;
-  
-  if ( randomQuote.hasOwnProperty('year') ) {
+
+  if (randomQuote.hasOwnProperty('citation')) {
+    html += `
+    <span class="citation">${randomQuote.citation}</span>
+    `;
+  }
+
+  if (randomQuote.hasOwnProperty('year')) {
     html += `
     <span class="year">${randomQuote.year}</span>
     `;
   }
 
-  if ( randomQuote.hasOwnProperty('citation') ) {
+  if (randomQuote.hasOwnProperty('tags')) {
     html += `
-    <span class="citation">${randomQuote.citation}</span>
+    <span class="tags">${randomQuote.tags}</span>
     `;
   }
-  
-  html += '</p>'
-  console.log(html);
 
+  html += '</p>'
+
+  document.getElementById('quote-box').innerHTML = html;
+  setBackgroundColor();
 }
 
-printQuote();
+/* This is where I found the information for the setInterval function.
+  https://stackoverflow.com/questions/32913226/auto-refresh-page-every-30-seconds#:~:text=If%20you%20really%20want%20to,(function%20()%20%7B%20window.
+  This function refreshes the printQuote function every 2000ms
+*/
+window.setInterval(function () {
+  printQuote();
+}, 2000);
+
+// This function returns a random number between 0 and 255 
+function randomColorNumber() {
+  return Math.floor(Math.random() * 255) + 1;
+}
+
+// This function calls randomColorNumber 3 times to get the r, g and b values and sets the body background color
+function setBackgroundColor() {
+  const r = randomColorNumber();
+  const g = randomColorNumber();
+  const b = randomColorNumber();
+
+  document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+}
+
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-// document.getElementById('load-quote').addEventListener("click", printQuote, false);
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
